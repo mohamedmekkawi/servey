a57a4f7d6b4f00602ee4ff47de754fa9d36e9517
BH210-18\ipd <ipd@ipd.com>
2017-10-03 14:05:43 -0400

    contact information question1

--- servey/servey/Question1.xaml.cs
+++ servey/servey/Question1.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace servey
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Question1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Question1 : Window
     {
-        public MainWindow()
+        public Question1()
         {
             InitializeComponent();
-        }
-
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
         }
     }
 }
